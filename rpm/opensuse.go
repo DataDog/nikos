bcfb5cd8c6209cbaf831125f2048f9e60962bf80
Isabelle Sauve <isabelle.sauve@datadoghq.com>
2021-10-07 10:37:15 -0400

    [opensuse] Generalize opensuse-leap detection

--- rpm/opensuse.go
+++ rpm/opensuse.go
@@ -2,6 +2,7 @@ package rpm
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 
 	"github.com/DataDog/nikos/rpm/dnf"
@@ -15,9 +16,6 @@ type OpenSUSEBackend struct {
 }
 
 func (b *OpenSUSEBackend) GetKernelHeaders(directory string) error {
-	// For OpenSUSE Leap, we first try with only the repo-oss and repo-update repositories
-	// If we don't find it, we use the full list of repositories
-
 	kernelRelease := b.target.Uname.Kernel
 
 	pkgNevra := "kernel"
@@ -29,23 +27,28 @@ func (b *OpenSUSEBackend) GetKernelHeaders(directory string) error {
 	pkgNevra += "-devel-" + kernelRelease
 
 	var disabledRepositories []*dnf.Repository
+	ossRepoRgx := regexp.MustCompile(`openSUSE-Leap-\d+.\d+-Oss`)
+	updateRepoRgx := regexp.MustCompile(`openSUSE-Leap-\d+.\d+-Update`)
+
+	// For OpenSUSE Leap, we first try with only the repo-oss and repo-update repositories
+	b.logger.Infof("Trying with Oss & Update repositories")
 	for _, repo := range b.dnfBackend.GetEnabledRepositories() {
 		if repo.Id != "repo-oss" && repo.Id != "repo-update" &&
-			repo.Id != "openSUSE-Leap-15.2-Oss" && repo.Id != "openSUSE-Leap-15.2-Update" {
+			!ossRepoRgx.MatchString(repo.Id) && !updateRepoRgx.MatchString(repo.Id) {
 			b.dnfBackend.DisableRepository(repo)
 			disabledRepositories = append(disabledRepositories, repo)
 		}
 	}
-
-	if err := b.dnfBackend.GetKernelHeaders(pkgNevra, directory); err != nil {
-		b.logger.Info("Retrying with the full set of repositories")
-		for _, repo := range disabledRepositories {
-			b.dnfBackend.EnableRepository(repo)
-		}
-		return b.dnfBackend.GetKernelHeaders(pkgNevra, directory)
+	err := b.dnfBackend.GetKernelHeaders(pkgNevra, directory)
+	if err == nil {
+		return nil
 	}
 
-	return nil
+	b.logger.Infof("Error downloading package: %s. Retrying with the full set of repositories", err)
+	for _, repo := range disabledRepositories {
+		b.dnfBackend.EnableRepository(repo)
+	}
+	return b.dnfBackend.GetKernelHeaders(pkgNevra, directory)
 }
 
 func NewOpenSUSEBackend(target *types.Target, reposDir string, logger types.Logger) (types.Backend, error) {
