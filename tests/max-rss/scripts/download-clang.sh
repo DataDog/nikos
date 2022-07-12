8766d99f480a934ddd01390e553e8cf595a4909e
Usama Saqib <usama.saqib@datadoghq.com>
2022-07-12 11:59:16 +0200

    downloading clang binary on each build

--- tests/max-rss/scripts/download-clang.sh
+++ tests/max-rss/scripts/download-clang.sh
@@ -6,7 +6,5 @@ MACHINE=$(uname -m)
 ARCH="${arch_mapping[${MACHINE}]}"
 
 CLANG_URL="https://dd-agent-omnibus.s3.amazonaws.com/llvm/clang-14.0.5.${ARCH}"
-if [ ! -f ./tools/clang-14 ]; then
-	wget $CLANG_URL -O ./tools/clang-14
-	chmod 0755 ./tools/clang-14
-fi
+wget $CLANG_URL -O ./tools/clang-14
+chmod 0755 ./tools/clang-14
